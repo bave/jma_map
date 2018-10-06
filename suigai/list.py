59ac26b246fab2f049a1dc06f6e1fe08c1bf461a
bave <inoue.tomoya@gmail.com>
2018-10-06 23:57:51 +0900

    update

--- suigai/list.py
+++ suigai/list.py
@@ -14,7 +14,7 @@ d = datetime.datetime.now(timezone('GMT'))
 d = d - datetime.timedelta(minutes=(d.minute%10+10), seconds=d.second, microseconds=d.microsecond)
 now = d.strftime("%Y%m%d%H%M")
 
-path = "https://www.jma.go.jp/jp/kaikotan/kaikotan_tile/KAIKOTAN10M/%s/%s/zoom%i/%i_%i.png"
+path = "https://www.jma.go.jp/jp/suigaimesh/suigai_tile/INUND/%s/%s/zoom%i/%i_%i.png"
 
 for i in range(int(sys.argv[1]), int(sys.argv[2])):
     for j in range(int(sys.argv[3]), int(sys.argv[4])):
